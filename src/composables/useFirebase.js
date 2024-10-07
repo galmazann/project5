977d1117c589111aefc11f57e2dc29732e2eab9a
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-10-06 23:15:44 -0500

    added template

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDu8KIpdaIf88sV65XSKR9j2dw2g9wJAto",
+  authDomain: "homework5-fd67c.firebaseapp.com",
+  projectId: "homework5-fd67c",
+  storageBucket: "homework5-fd67c.appspot.com",
+  messagingSenderId: "948626692391",
+  appId: "1:948626692391:web:2a94cf1f685fc7ef19f102"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
